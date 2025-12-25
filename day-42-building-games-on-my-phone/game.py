7fcc00428bbc14b3b4a597cbafd380cef0d1d82d
Caleb Wodi <calebwodi33@gmail.com>
2025-12-25 15:42:20 -0800

    Update game.py

--- day-42-building-games-on-my-phone/game.py
+++ day-42-building-games-on-my-phone/game.py
@@ -7,7 +7,7 @@ pygame.init()
 # Full-screen setup
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 WIDTH, HEIGHT = screen.get_size()
-pygame.display.set_caption("Day 42 - Shooting Game 🎮🔥")
+pygame.display.set_caption("Day 42: Shooting Game 🎮🔥")
 clock = pygame.time.Clock()
 
 # Colors
@@ -206,3 +206,4 @@ while running:
 
     pygame.display.flip()
     clock.tick(30)
+
